fa14e2a27fa917a31c81373088ed1c180e8bcb21
sony2909 <ssoni813704@gmail.com>
2025-02-03 13:31:52 +0530

    first commit

---
+++ friend.java
@@ -0,0 +1,8 @@
+public class Friend{
+
+	public static void main(String args[]){
+		System.out.println("sony");
+		System.out.println("degree");
+		System.out.println("srisailam");
+	}
+}
---
+++ javaprogram.java
@@ -0,0 +1,7 @@
+public class java program{
+	
+{
+public static void main(string args[])
+System.out.println("my first java program");
+}
+}
