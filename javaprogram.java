0716b66d901f2237af89c3330192a581258b7539
sony2909 <ssoni813704@gmail.com>
2025-02-10 10:26:49 +0530

    hello

--- javaprogram.java
+++ javaprogram.java
@@ -1,7 +1,7 @@
-public class java program{
+public class javaprogram {
 	
-{
-public static void main(string args[])
+
+public static void main(String args[]){
 System.out.println("my first java program");
 }
 }
